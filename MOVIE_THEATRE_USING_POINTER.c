fadd21a5507e89c932600b5b04f70b2131373d45
itsvishwa23 <itsvishwa23@gmail.com>
2024-06-01 10:35:31 +0530

    :tada:

--- MOVIE_THEATRE_USING_POINTER.c
+++ MOVIE_THEATRE_USING_POINTER.c
@@ -7,17 +7,18 @@ struct Movie {
     char category[50];     
 };
 
-void getmoviedetails(struct Movie *m);
-void getcircledetails(char *circle_details);
-void CalculateTicketCost(struct Movie *m, char *circle);
+void getmoviedetails(struct Movie*);
+void getcircledetails(char[]);
+int CalculateTicketCost(struct Movie*, char*);
 
 int main() {
     struct Movie m;
     char circle_details[100];
-    
+    int a;
     getmoviedetails(&m);
     getcircledetails(circle_details);
-    CalculateTicketCost(&m, circle_details);
+    a=CalculateTicketCost(&m, circle_details);
+    printf("%d",a);
     
     return 0;
 }
@@ -39,29 +40,33 @@ void getcircledetails(char *circle_details) {
 
     printf("Circle Details: %s\n", circle_details);
 }
-
-void CalculateTicketCost(struct Movie *m, char *circle) {
+int CalculateTicketCost(struct Movie *m, char *circle) {
     if (strcmp(circle, "gold") == 0 && strcmp(m->category, "2d") == 0) {
         printf("Movie Name: %s\n", m->movie_name);
         printf("Duration: %d\n", m->duration);
         printf("Category: %s\n", m->category);
         printf("Ticket cost is Rs. 300\n");
+        return 1;
     } else if (strcmp(circle, "gold") == 0 && strcmp(m->category, "3d") == 0) {
         printf("Movie Name: %s\n", m->movie_name);
         printf("Duration: %d\n", m->duration);
         printf("Category: %s\n", m->category);
         printf("Ticket cost is Rs. 500\n");
+        return 1;
     } else if (strcmp(circle, "silver") == 0 && strcmp(m->category, "2d") == 0) {
         printf("Movie Name: %s\n", m->movie_name);
         printf("Duration: %d\n", m->duration);
         printf("Category: %s\n", m->category);
         printf("Ticket cost is Rs. 150\n");
+        return 1;
     } else if (strcmp(circle, "silver") == 0 && strcmp(m->category, "3d") == 0) {
         printf("Movie Name: %s\n", m->movie_name);
         printf("Duration: %d\n", m->duration);
         printf("Category: %s\n", m->category);
         printf("Ticket cost is Rs. 400\n");
+        return 1;
     } else {
         printf("Invalid input\n");
+        return -1;
     }
 }
